eb654764ac759110517edaf23ffbab851d1b9915
Crowdbotics <engineering@crowdbotics.com>
2023-03-09 10:13:39 +0000

    Update store.

--- store/ravinderconnector/api.js
+++ store/ravinderconnector/api.js
@@ -1,14 +1,7 @@
 import axios from "axios"
-import {
-  RAVINDER_CONNECTOR_USERNAME,
-  RAVINDER_CONNECTOR_PASSWORD
-} from "react-native-dotenv"
+import {} from "react-native-dotenv"
 const ravinderconnector = axios.create({
   baseURL: "https://www.google.com/",
-  auth: {
-    username: RAVINDER_CONNECTOR_USERNAME,
-    password: RAVINDER_CONNECTOR_PASSWORD
-  },
   headers: { Accept: "application/json", "Content-Type": "application/json" }
 })
 export const apiService = {}
